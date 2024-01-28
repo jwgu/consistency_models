2ac722dd9cd06d1a049d04341e9deb80ae114eb6
Jinwei Gu <gujinwei@gmail.com>
2024-01-28 12:12:57 +0800

    edit

--- train.py
+++ train.py
@@ -358,15 +358,10 @@ class ImprovedConsistencyModelTrainingConfig:
     model_ckpt_path: str = "checkpoints/icm"
     resume_ckpt_path: Optional[str] = None
 
-
-
-
-
 #------------------- main interface --------------------   
-
 def show_unet():
     # for debug purpose
-    summary(UNet(UNetConfig()), input_size=((0, 3, 32, 32), (1,)))
+    summary(UNet(UNetConfig()), input_size=((1, 3, 32, 32), (1,)))
 
 
 def train_consistency_model():
@@ -385,7 +380,7 @@ def train_consistency_model():
             sample_every_n_steps=999, lr_scheduler_iters=1000
         ),
         trainer=Trainer(
-            max_steps=9999,
+            max_steps=10000,
             log_every_n_steps=9,
             logger=TensorBoardLogger(".", name="logs", version="cm"),
             callbacks=[LearningRateMonitor(logging_interval="step")],
@@ -472,90 +467,18 @@ def train_consistency_model_improved():
     lit_icm.model.save_pretrained(config.model_ckpt_path)
 
 
+import argparse
 
-#------------------------------------------------------------------------------------------
-def plot_images(images: Tensor, cols: int = 4) -> None:
-    rows = max(images.shape[0] // cols, 1)
-    fig, axs = plt.subplots(rows, cols)
-    axs = axs.flatten()
-    for i, image in enumerate(images):
-        axs[i].imshow(image.permute(1, 2, 0).numpy() / 2 + 0.5)
-        axs[i].set_axis_off()
-        
-def sample_consistency_model():
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    dtype = torch.bfloat16 if torch.cuda.is_available() else torch.float32
-    unet = UNet.from_pretrained("checkpoints/cm").eval().to(device=device, dtype=dtype)
-    
-    dm = ImageDataModule(ImageDataModuleConfig("butterflies256", batch_size=4))
-    dm.setup()
-    
-    batch, _ = next(iter(dm.train_dataloader()))
-    batch = batch.to(device=device, dtype=dtype)
-    plot_images(batch.float().cpu())
-
-    consistency_sampling_and_editing = ConsistencySamplingAndEditing()
-
-    with torch.no_grad():
-        samples = consistency_sampling_and_editing(
-            unet,
-            torch.randn((4, 3, 32, 32), device=device, dtype=dtype),
-            sigmas=[80.0],  # Use more steps for better samples e.g 2-5
-            clip_denoised=True,
-            verbose=True,
-        )
-
-    plot_images(samples.float().cpu())
-
-
-    # inpaint
-    random_erasing = T.RandomErasing(p=1.0, scale=(0.2, 0.5), ratio=(0.5, 0.5))
-    masked_batch = random_erasing(batch)
-    mask = torch.logical_not(batch == masked_batch)
-
-    plot_images(masked_batch.float().cpu())
-
-    with torch.no_grad():
-        inpainted_batch = consistency_sampling_and_editing(
-            unet,
-            masked_batch,
-            sigmas=[5.23, 2.25],
-            mask=mask.to(dtype=dtype),
-            clip_denoised=True,
-            verbose=True,
-        )
-
-    plot_images(torch.cat((masked_batch, inpainted_batch), dim=0).float().cpu())
-
-
-    # interpolate
-    batch_a = batch.clone()
-    batch_b = torch.flip(batch, dims=(0,))
-    plot_images(torch.cat((batch_a, batch_b), dim=0).float().cpu()) 
-
-    with torch.no_grad():
-        interpolated_batch = consistency_sampling_and_editing.interpolate(
-            unet,
-            batch_a,
-            batch_b,
-            ab_ratio=0.5,
-            sigmas=[5.23, 2.25],
-            clip_denoised=True,
-            verbose=True,
-        )
-
-    plot_images(torch.cat((batch_a, batch_b, interpolated_batch), dim=0).float().cpu())
-    
-    
-    
 if __name__=='__main__':
 
-   show_unet()
-
-   #train_consistency_model_train()
-
-   #train_consistency_model_improved()
-
-   #sample_consistency_model()
-
-   #sample_consistency_model_improved()
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument('-m','--mode', type=str, help="mode of show, cm, or icm")
+    args = parser.parse_args()
+    
+    if args.mode=='show':
+        show_unet()
+    elif args.mode=='cm':
+        train_consistency_model()
+    elif args.mode=='icm':
+        train_consistency_model_improved()
